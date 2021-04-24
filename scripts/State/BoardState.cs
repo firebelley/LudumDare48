2a864e86fdbe4c7a9a31ba4e57f2d27cfcba2a1f
Firebelley <firebelley@users.noreply.github.com>
2021-04-23 23:21:18 -0400

    basic tile validation and placement

--- scripts/State/BoardState.cs
+++ scripts/State/BoardState.cs
@@ -1,10 +1,14 @@
+using Game.Data;
+using Godot;
 using GodotUtilities.StateManagement;
 
 namespace Game.State
 {
     public class BoardState : IState
     {
-        public bool BuildingSelected;
+        public SelectedBuildingInfo SelectedBuildingInfo;
         public int ResourceCount;
+        public Vector2 HoveredTile;
+        public bool TilePlacementValid;
     }
 }
---
+++ scripts/Util/MouseDispatcher.cs
@@ -0,0 +1,29 @@
+using Game.State;
+using Godot;
+using GodotUtilities;
+
+namespace Game.Util
+{
+    public class MouseDispatcher : Node
+    {
+        private Vector2 hoveredTile = Vector2.One * -1;
+
+        private TileMap tileMap;
+
+        public override void _EnterTree()
+        {
+            this.WireNodes();
+            tileMap = this.GetAncestor<BaseLevel>().TileMap;
+        }
+
+        public override void _Process(float delta)
+        {
+            var mouseTilePos = tileMap.WorldToMap(tileMap.GetGlobalMousePosition());
+            if (mouseTilePos != hoveredTile)
+            {
+                hoveredTile = mouseTilePos;
+                GameState.BoardStore.DispatchAction(new BoardActions.TileHovered { Tile = hoveredTile });
+            }
+        }
+    }
+}
