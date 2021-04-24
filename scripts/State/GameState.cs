4421298e379bfee6078cc19011f4ade33434322b
Firebelley <firebelley@users.noreply.github.com>
2021-04-24 13:01:37 -0400

    remove unused files

--- scripts/State/GameState.cs
+++ scripts/State/GameState.cs
@@ -4,7 +4,6 @@ namespace Game.State
 {
     public class GameState : StateManager
     {
-        public static Store<PlayerState> PlayerStore { get; private set; } = new(new PlayerReducer());
         public static Store<BoardState> BoardStore { get; private set; } = new(new BoardReducer());
     }
 }
