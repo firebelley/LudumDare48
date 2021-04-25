55f725101025a4138c3572ec011a707dfcf106ec
Firebelley <firebelley@users.noreply.github.com>
2021-04-25 11:29:16 -0400

    add victory particles

--- scripts/GameObject/Goal.cs
+++ scripts/GameObject/Goal.cs
@@ -1,4 +1,5 @@
 using Game.Level;
+using Game.State;
 using Godot;
 using GodotUtilities;
 
@@ -8,14 +9,26 @@ namespace Game.GameObject
     {
         public Vector2 TilePosition { get; private set; }
 
+        [Node]
+        private Particles2D victoryParticles;
+        [Node]
+        private Particles2D victoryParticlesExplosive;
+
         public override void _EnterTree()
         {
             this.WireNodes();
+            GameState.CreateEffect<BoardActions.Complete>(this, nameof(CompleteEffect));
         }
 
         public override void _Ready()
         {
             TilePosition = this.GetAncestor<BaseLevel>().TileMap.WorldToMap(GlobalPosition);
+        }
+
+        private void CompleteEffect(object _)
+        {
+            victoryParticles.Emitting = true;
+            victoryParticlesExplosive.Emitting = true;
         }
     }
 }
